88c333954a7d8a93444c9fab2361cf1af612c2ac
Khushi753 <khushisrivastav0808@gmail.com>
2024-10-13 11:37:09 +0530

    modified valid js file

--- Day4/valid.js
+++ Day4/valid.js
@@ -1,12 +1,19 @@
-function validate(){
-    let a=document.getElementById("un").value;
-    let b=document.getElementById("pass").value;
-    if(a.length>10 && a!='' ) {
-        
+function validate() {
+    let a = document.getElementById("un").value;
+    let b = document.getElementById("pass").value;
+
+   
+    if (a.length < 10 || a === '') {
+        alert("login failed.");
+        return false;  
+    }
+
+  
+    if (b === '') {
+        alert("Password cannot be empty.");
         return false;
-        
-     }
-    // else{
-    //     alert("not sucess");
-    // }
+    }
+
+  
+    return true;
 }

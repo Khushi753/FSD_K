2edef353fbcec064e35ef44b93139af7420b1a5a
Khushi753 <khushisrivastav0808@gmail.com>
2024-10-15 15:38:29 +0530

    DOM

---
+++ Day5/script.js
@@ -0,0 +1,16 @@
+// let a=confirm("do yu want compile");
+// alert(a)
+// let a=prompt("do you want","raj");
+// alert(a)
+// let disp=()=>{
+//  console.log("welcome to dimension");
+// };
+// disp();
+// let say=()=>console.log("arrow function");
+// say();
+let sum=(x,y)=>{
+    return x+y;
+
+};
+let res=sum(23,54);
+console.log(res);
